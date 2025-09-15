fa77d5564dc64faae58dd47caf9a5c25ff6fd265
Alex <kibgheeo775533@gmail.com>
2025-09-15 23:07:35 +0500

    practice mini tasks

---
+++ Python-Practice.py
@@ -0,0 +1,58 @@
+#Print Only Even Numbers
+# num_list = [1,2,3,4,5,6,7,8,9,10]
+
+# for n in num_list:
+#     if n%2==0:
+#         print(n)
+
+
+
+
+# #Remove all list items that contain 'a' in them
+# fruits = ["apple" , "banana", "kiwi", "grapes" , "imli"]
+
+# #List Comprehension
+# fruits = [val for val in fruits if "a" not in val]
+# print(fruits)
+
+
+
+
+# #Reverse sorting a list
+# nums = [5,2,1,9]
+# nums.sort(reverse=True)
+# print(nums)
+
+
+
+# #Copy a list, update it and original should not change
+# original = ["Biology" , "Computer" , "Maths"]
+# copy = original.copy()
+# copy[0] = "English"
+
+# print(copy)
+# print(original)
+
+
+
+
+# #Make Tuple and Unpack it into separate variable for each item
+# t = (10,20,30,40,50)
+# a,b,c,d,e = t
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+
+
+
+#Join two tuples into One Tuple
+# t1 = (1,2,3)
+# t2 = (4,5,6)
+# mixed = t1 + t2
+# print(mixed)
+
+
+

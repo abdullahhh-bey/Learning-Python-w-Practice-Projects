76865b2d2b244f0819c9deee9570a80e20e50a7c
Alex <kibgheeo775533@gmail.com>
2025-09-18 19:20:34 +0500

    Practice OOP

---
+++ Inheritance-Python.py
@@ -0,0 +1,21 @@
+# #Base Class
+# class Animal:
+#     legs = 4
+#     def __init__(self):
+#         pass
+    
+#     def make_sound(self):
+#         print("Some Sound")
+        
+# class Dog(Animal):
+    
+#     def __init__(self):
+#         pass
+
+#     def make_sound(self):
+#         print("Voff Voff")
+        
+# d1 = Dog()
+# d1.legs = 6
+# d1.make_sound()
+# print(d1.legs)
---
+++ OOP-Python.py
@@ -0,0 +1,40 @@
+# class Car:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+    
+#     def carInfo(self):
+#         print(f"Car Brand: {self.brand}\nCar Name: {self.model}")
+      
+#     def driving(self):
+#           print(f"{self.model} is moving")
+        
+# c1 = Car("BMW" , "MegMG")
+# c1.carInfo()
+# c1.driving()
+
+
+
+
+
+
+
+# #Student Class 
+# class Student:
+#     def  __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+        
+#     def is_passed(self):
+#         if self.marks >= 50:
+#             print("Passed")
+#         else:
+#             print("Failed")
+            
+# s1 = Student("Pingu" , 51)
+# s2 = Student("Hopi" , 90)
+# s3 = Student("Alex" , 40)
+
+# s1.is_passed()
+# s2.is_passed()
+# s3.is_passed()

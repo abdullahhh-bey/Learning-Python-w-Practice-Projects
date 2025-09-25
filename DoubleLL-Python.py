c8843c9c8c703f978ca5ab1c947c94c4f23957cd
Alex <kibgheeo775533@gmail.com>
2025-09-25 19:49:21 +0500

    DLL removeitem/IsEmpty fnc

--- DoubleLL-Python.py
+++ DoubleLL-Python.py
@@ -29,6 +29,20 @@ class DoubleLL:
         node.prev = temp
         
         
+    def insert_beginning(self , data):
+        node = Node(data)
+        temp = self.head 
+        if temp == None:
+            self.head = node
+        
+        node.next = temp
+        temp.prev = node 
+        self.head = node 
+        
+    
+    
+        
+        
     def IsEmpty(self):
         return self.head == None
        
@@ -37,15 +51,47 @@ class DoubleLL:
         temp = self.head
         if self.IsEmpty():
             print("Empty List!")
-            return
-        
+            return 
         while temp:
             print(temp.data , end=" ")
             temp = temp.next
+        
+        print("\n")
+    
+    
+    
+    def removeItem(self,key):
+        temp = self.head
+        if self.IsEmpty():
+            print("Empty")
+            return
+        if temp.data == key:
+            self.head = temp.next
+            self.head.prev = None
+            return
+
+        previous = temp
+        while temp.next:
+            temp = temp.next
+            if temp.data == key:
+                previous.next = temp.next
+                temp.next.prev = previous
+                print("Deleted")
+                return
+            previous = temp
+            
+        print("Not Found")
+        
+        
         
 dl = DoubleLL()
 dl.displayItems()
 dl.insertRear(1)
 dl.insertRear(2)
 dl.insertRear(1)
+dl.displayItems()
+dl.insert_beginning(10)
+dl.displayItems()
+dl.removeItem(1)
+dl.removeItem(11)
 dl.displayItems()

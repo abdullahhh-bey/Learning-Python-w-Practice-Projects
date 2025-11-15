44a087748d13d18ed4b62a89618cd2eb8140ad66
Alex <kibgheeo775533@gmail.com>
2025-11-15 18:57:29 +0500

    Added changes to map, filter and list comp

--- list_method.py
+++ list_method.py
@@ -3,5 +3,18 @@ def list_sum(data : list[int]) -> int:
     return result
 
 
+#List comprehension
+def list_sqaures(data : list[int]):
+    updatedList = [v*v for v in data]
+    list_iterator(updatedList)
+    
+    
+
+def list_iterator(data : list[int]):
+    for i in data:
+        print(f"{i}")
+    
+
 l = [1,2,3,4,5]
 print(f"List Answer: {list_sum(l)}")
+list_sqaures(l)
